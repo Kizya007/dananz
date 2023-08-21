73373af193b057067a915f8c7077fa24b41be01e
kizya007 <kizya007@mail.ru>
2023-08-21 10:50:57 +0200

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-
+  <div>
+    <headerDananz />
+  </div>
 </template>
 
 <script>
-
+import headerDananz from "./components/headerDananz.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    headerDananz,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
---
+++ src/components/headerDananz.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <nav class="navigation">
+      <ul class="navigation-list">
+        <li class="navigation-list__item"> <a href="">Home</a></li>
+        <li class="navigation-list__item"> <a href="">About Us</a></li>
+        <li class="navigation-list__item"> <a href="">Services</a></li>
+        <li class="navigation-list__item"> <a href="">Our Teams</a></li>
+        <li class="navigation-list__item"><a href="">Contact Us</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/header.scss";
+</style>
+
+<script></script>
