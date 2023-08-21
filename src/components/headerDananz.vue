f17ece469d348bb13c3000a922d36eceb73e7506
kizya007 <kizya007@mail.ru>
2023-08-21 11:52:38 +0200

    create header and adaptive

--- src/components/headerDananz.vue
+++ src/components/headerDananz.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header">
+    <img class="header-logo" src="../assets/img/logo.png" alt="">
     <nav class="navigation">
       <ul class="navigation-list">
         <li class="navigation-list__item"> <a href="">Home</a></li>
@@ -9,6 +10,7 @@
         <li class="navigation-list__item"><a href="">Contact Us</a></li>
       </ul>
     </nav>
+    <button class="header-btn">Menu</button>
   </div>
 </template>
 
