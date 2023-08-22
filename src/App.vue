ecc4becc0568d724699d2bbfb05f2124cc7dc3e8
kizya007 <kizya007@mail.ru>
2023-08-22 12:09:06 +0200

    create components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-
+  <aboutInfo />
 </template>
 
 <script>
-
+import aboutInfo from "./components/aboutInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    aboutInfo,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
---
+++ src/components/aboutInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <div class="name-section">
+      <span class="">About </span>
+      <h2>
+        “We're one of the best furniture agency. Prioritizing customers and
+        making purchases easy are the hallmarks of our agency.”
+      </h2>
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/about.scss";
+</style>
+
+<script></script>
