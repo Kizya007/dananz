69926ef4be243b41cd25a56b39427a06e1155517
kizya007 <kizya007@mail.ru>
2023-08-22 14:20:30 +0200

    create components about

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,6 @@ export default {
 </script>
 
 <style>
+@import "./assets/global.scss";
 @import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
--- src/components/aboutInfo.vue
+++ src/components/aboutInfo.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="about">
-    <div class="name-section">
-      <span class="">About </span>
-      <h2>
+    <div class="up-block">
+      <span class="up-block__name">ABOUT</span>
+      <h2 class="up-block__title">
         “We're one of the best furniture agency. Prioritizing customers and
         making purchases easy are the hallmarks of our agency.”
       </h2>
     </div>
-
-    <div></div>
+    <div class="bottom-block">
+      <img class="bottom-block__img" src="../assets/img/aboutImg.png" alt="" />
+      <div class="reviews-block">
+        <div class="reviews-block__img">
+          <img src="../assets/img/aboutReviews.png" alt="" />
+        </div>
+        <div class="reviews-block__text">
+          <p class="name">Arga Danaan</p>
+          <p class="position">CEO of Dananz</p>
+        </div>
+      </div>
+      <div class="right-block">
+        <p class="right-block__text">
+          Online learning with us does not interfere with your daily life.
+          because learning can be done anytime and anywhere.
+        </p>
+        <button class="right-block__btn">Learn More</button>
+      </div>
+    </div>
   </div>
 </template>
 
