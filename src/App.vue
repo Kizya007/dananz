50db19c59ea20cdfaced073f8175bad16d9fb7a1
kizya007 <kizya007@mail.ru>
2023-08-23 09:56:34 +0200

    create components service

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-
+  <serviceInfo />
 </template>
 
 <script>
-
+import serviceInfo from "./components/serviceInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    serviceInfo,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
-
+@import "./assets/global.scss";
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
