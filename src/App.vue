4222a1c3764fa87b6ef3debf9035a6868ef743bb
kizya007 <kizya007@mail.ru>
2023-08-28 18:00:58 +0200

    create components and adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-
+  <materialInfo />
 </template>
 
 <script>
-
+import materialInfo from "./components/materialInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    materialInfo,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
---
+++ src/components/materialInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="material">
+    <div class="left-block">
+      <p class="left-block__name">MATERIAL</p>
+      <h2 class="left-block__title">
+        choice of materials for quality furniture.
+      </h2>
+      <p class="left-block__subtitle">
+        You can custom the material as desired. And our furniture uses the best
+        materials and selected quality materials.
+      </p>
+      <button class="left-block__btn">See Materials</button>
+    </div>
+    <div class="right-block">
+      <div class="right-block__one">
+        <img src="../assets/img/imageOneMaterial.png" alt="" />
+      </div>
+      <div class="right-block__column">
+        <img class="img-two" src="../assets/img/imageTwoMaterial.png" alt="" />
+        <img
+          class="img-three"
+          src="../assets/img/imageThreeMaterial.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/Material.scss";
+@import "../assets/global.scss";
+</style>
+
+<script></script>
