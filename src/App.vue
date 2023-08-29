c8ee2e461340a4667b84e29cfe32bb66a5587bb1
kizya007 <kizya007@mail.ru>
2023-08-29 10:05:06 +0200

    create components contact and adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-
+  <contactUs />
 </template>
 
 <script>
-
+import contactUs from "./components/contactUs.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    contactUs,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
---
+++ src/components/contactUs.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contact">
+    <h2 class="contact-title">
+      let's discuss making your interior <br> like a dream place!
+    </h2>
+    <div class="contact-description">
+      <p class="contact-description__text">
+        Contact us below to work together to build your amazing interior
+      </p>
+      <button class="contact-description__btn">Contact Us</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/Contact.scss";
+</style>
+
+<script></script>
