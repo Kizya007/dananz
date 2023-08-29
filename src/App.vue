e47dc9a936ad703518bbd28dbd97241ec4de49cf
kizya007 <kizya007@mail.ru>
2023-08-29 12:12:36 +0200

    create footer and adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-
+  <footerInfo />
 </template>
 
 <script>
-
+import footerInfo from "./components/footerInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    footerInfo,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap");
 </style>
---
+++ src/assets/icon/rightLinkIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="26"
+    height="12"
+    viewBox="0 0 26 12"
+    fill="none"
+  >
+    <path d="M0 10.2479H23.5L16 1.23633" stroke="#141414" stroke-width="2" />
+  </svg>
+</template>
